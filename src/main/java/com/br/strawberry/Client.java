8c6bb1cda0efe31f2b9a326b4d949e220e562512
GabrielMartins <102445948+GabriellMartins@users.noreply.github.com>
2024-07-18 18:46:53 -0300

    first commit
    first commit

---
+++ src/main/java/com/br/strawberry/Client.java
@@ -0,0 +1,42 @@
+package com.br.strawberry;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+    
+    private Socket socket;
+    private BufferedReader input;
+    private PrintWriter output;
+
+    public Client(String address, int port) {
+        try {
+            socket = new Socket(address, port);
+            input = new BufferedReader(new InputStreamReader(System.in));
+            output = new PrintWriter(socket.getOutputStream(), true)/
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void start() {
+        String message;
+        try {
+            while ((message = input.readLine()) != null) {                output.println(message);
+            }
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client("localhost", 5000);
+        client.start();
+    }
+}
